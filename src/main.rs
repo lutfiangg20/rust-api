7ce49229aa63353a246fabf48122f34bfcda8f20
lutfi angga <lutfiangg20@gmail.com>
2025-09-01 23:58:30 +0700

    .

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(users::scope())
             .service(categories::scope())
-        // .service(user_controller::hello)
-        // .service(user_controller::users)
-        // .service(user_controller::create_user)
     })
     .bind(("0.0.0.0", 3000))?
     .run()
